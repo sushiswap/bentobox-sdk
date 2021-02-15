7ee3c6cec8c7ea23ab097b1ff1e4fbe1f0ddfe4f
BoringCrypto <b@rtje.net>
2021-02-15 20:38:37 +1100

    Use BoringSolidity

--- contracts/IBentoBoxV1.sol
+++ contracts/IBentoBoxV1.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.6.12;
 pragma experimental ABIEncoderV2;
 
-import './IERC20.sol';
+import '@boringcrypto/boring-solidity/contracts/interfaces/IERC20.sol';
 import './IBatchFlashBorrower.sol';
 import './IFlashBorrower.sol';
 import './IStrategy.sol';
--- contracts/IFlashBorrower.sol
+++ contracts/IFlashBorrower.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.6.12;
-import './IERC20.sol';
+import '@boringcrypto/boring-solidity/contracts/interfaces/IERC20.sol';
 
 interface IFlashBorrower {
     function onFlashLoan(
